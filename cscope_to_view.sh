6b771ebc3c3005e58685a3eeb263a80a552527d4
wobakj <wobakj@web.de>
2023-09-30 20:45:59 +0200

    adjust scrpt

--- cscope_to_view.sh
+++ cscope_to_view.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-python3 ./callcallgraph.py $1/cscope.out
-dot -Grankdir=LR -Tpng -O $1/callgraph.dot
-dot -Grankdir=LR -Tpng -O $1/filegraph.dot
-dot -Grankdir=LR -Tpng -O $1/foldergraph.dot
-xdg-open $1/foldergraph.dot.png
+python3 ./callcallgraph.py --graph=$2 $1/cscope.out
+dot -Grankdir=LR -Tpng -O $1/$2graph.dot
+xdg-open $1/$2graph.dot.png
 # xdg-open $1/filegraph.dot.png
 # xdg-open $1/callgraph.dot.png
